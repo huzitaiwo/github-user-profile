1585d48bcafff2a53386ea800e250a0e7b0fea4f
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2022-02-23 09:52:48 +0100

    updated App component

--- src/App.js
+++ src/App.js
@@ -1,39 +1,12 @@
-import { BrowserRouter, Switch, Route  } from "react-router-dom"
-
 // styles
 import "./App.css"
 
 // pages and components
-import Home from './pages/home/Home'
-import Recipe from './pages/recipe/Recipe'
-import Search from './pages/search/Search'
-import Create from './pages/create/Create'
-import Navbar from "./components/Navbar"
-import Profile from "./pages/profile/Profile"
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/create">
-            <Create />
-          </Route>
-          <Route path="/search">
-            <Search />
-          </Route>
-          <Route path="/profile">
-            <Profile />
-          </Route>
-          <Route path="/recipes/:id">
-            <Recipe />
-          </Route>
-        </Switch>
-      </BrowserRouter>
+      App component
     </div>
   );
 }
