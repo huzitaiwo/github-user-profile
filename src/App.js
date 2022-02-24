89e1bb3a3c48a589a72bed287d8a42c4d23a41b7
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2022-02-24 10:11:15 +0100

    created pages folder

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+// import { useFetch } from './hooks/useFetch'
+
 // styles
 import "./App.css"
 
 // pages and components
 
 function App() {
+  const { data, error, isPending } = useFetch('http://localhost:5000/posts')
+
   return (
     <div className="App">
-      App component
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/'>
+
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
