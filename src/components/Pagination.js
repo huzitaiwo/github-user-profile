47cd1dff200819caf1ef1a14775250518bef9594
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2022-02-25 18:08:20 +0100

    added getPaginatedData function

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -3,24 +3,26 @@ import './Pagination.css'
 import React from 'react'
 
 export default function Pagination({ data, RenderComponent, title, pageLimit, dataLimit }) {
-  const [pages] = useState(Math.round(data.length / dataLimit));
-  const [currentPage, setCurrentPage] = useState(1);
+  const [pages] = useState(Math.round(data.length / dataLimit))
+  const [currentPage, setCurrentPage] = useState(1)
 
   function goToNextPage() {
     setCurrentPage(page => page + 1)
   }
 
   function goToPreviousPage() {
-    setCurrentPage(page => page - 1);
+    setCurrentPage(page => page - 1)
   }
 
   function changePage(event) {
-    const pageNumber = Number(event.target.textContent);
-    setCurrentPage(pageNumber);
+    const pageNumber = Number(event.target.textContent)
+    setCurrentPage(pageNumber)
   }
 
   const getPaginatedData = () => {
-     // not yet implemented
+    const startIndex = currentPage * dataLimit - dataLimit
+    const endIndex = startIndex + dataLimit
+    return data.slice(startIndex, endIndex)
   }
 
   const getPaginationGroup = () => {
