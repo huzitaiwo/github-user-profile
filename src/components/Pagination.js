664c8a64eb71f9c1bd4b7415e769b6989c2b19ce
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2022-02-25 17:58:03 +0100

    Pagination.js

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,1 +1,34 @@
 import './Pagination.css'
+
+import React from 'react'
+
+export default function Pagination() {
+  const [pages] = useState(Math.round(data.length / dataLimit));
+  const [currentPage, setCurrentPage] = useState(1);
+
+  function goToNextPage() {
+     // not yet implemented
+  }
+
+  function goToPreviousPage() {
+     // not yet implemented
+  }
+
+  function changePage(event) {
+     // not yet implemented
+  }
+
+  const getPaginatedData = () => {
+     // not yet implemented
+  }
+
+  const getPaginationGroup = () => {
+     // not yet implemented
+  }
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
