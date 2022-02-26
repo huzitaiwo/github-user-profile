e97b35532b437f28be95a1b2c599a11c7210d64b
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2022-02-26 22:07:45 +0100

    added github icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,16 @@
-// styles
+// styles and images
 import './Navbar.css'
+import githubBrand from '../icons/github-brands.svg'
+
+// components
 import Searchbar from './Searchbar'
+
 
 export default function Navbar() {
   return (
     <nav className='navbar'>
       <div className="container">
-        <div className="logo">Github user profiles</div>
+        <div className="logo"><img src={githubBrand} alt="" /></div>
         <Searchbar />
       </div>
     </nav>
