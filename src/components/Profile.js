a9735bc53421d80a01e2f5337ea6f85a70f50f7c
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2022-02-27 00:45:04 +0100

    fixing layout

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -12,7 +12,7 @@ export default function Profile({ data }) {
     <div className='container profile'>
       {data.items.map(profile => (
         <div key={profile.id} className='card'>
-          {/* <img src={profile.avatar_url} alt="" /> */}
+          <img src={profile.avatar_url} alt="" />
           <p><span className='label'>username:</span> {profile.login}</p>
           <p><span className='label'>score:</span> {profile.score}</p>
           <p><span className='label'>profile type:</span> {profile.type}</p>
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -22,21 +22,21 @@ import './Home.css'
 
 
 export default function App() {
-  const {data, isPending, error } = useFetch('https://api.github.com/search/users?q=lagos&page=1')
+  const { data, isPending, error } = useFetch('https://api.github.com/search/users?q=lagos&page=1')
   
   return (
     <div className='container'>
       {error && <h2>{error}</h2>}
       {isPending && <h2>Loading...</h2>}
-      {/* {data && <Profile data={data} />} */}
-      {data && (
+      {data && <Profile data={data} />}
+      {/* {data && (
         <Pagination
           data={data}
           RenderComponent={Profile}
           pageLimit={5}
           dataLimit={10}
         />
-      )}
+      )} */}
     </div>
   );
 }
