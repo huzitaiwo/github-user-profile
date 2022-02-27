8efc6aaf08b8197a108189ce2864f8191cb2b236
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2022-02-27 01:36:51 +0100

    imported useRef hook

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,10 +1,18 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 
 // styles
 import './Profile.css'
 
 export default function Profile({ data }) {
   const [hover, setHover] = useState(false)
+  const cardRef = useRef(null)
+
+  const handleMouseEnter = (e) => {
+    console.log(cardRef.current)
+    if(cardRef.current == e.target) {
+      setHover(true)
+    }
+  }
 
   if(data.length === 0) {
     console.log('no user')
@@ -14,13 +22,16 @@ export default function Profile({ data }) {
   return (
     <div className='container profile'>
       {data.items.map(profile => (
-
-        <div key={profile.id} className='card'>
-          <img src={profile.avatar_url} alt="" />
+        <div key={profile.id} className='card' onMouseEnter={handleMouseEnter} ref={cardRef}>
+          {/* <img src={profile.avatar_url} alt="" /> */}
           <p><span className='label'>username:</span> {profile.login}</p>
-          <p><span className='label'>score:</span> {profile.score}</p>
-          <p><span className='label'>profile type:</span> {profile.type}</p>
-          <a className='link' href={profile.html_url}>View profile</a>
+          {hover && (
+          <>
+            <p><span className='label'>score:</span> {profile.score}</p>
+            <p><span className='label'>profile type:</span> {profile.type}</p>
+            <a className='link' href={profile.html_url}>View profile</a>
+          </>
+          )}
         </div>
       ))}
     </div>
