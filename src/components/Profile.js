645b5a1fe130f63b915384f3589f41207c46e7de
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-02-28 23:06:45 +0100

    styled button

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,7 +3,7 @@ import { useState } from 'react'
 // styles
 import './Profile.css'
 
-export default function Profile({ data }) {
+export default function Profile({ data, totalPages, page, setPage, isPending }) {
   const [hover, setHover] = useState(false)
 
   if(data.length === 0) {
@@ -11,26 +11,29 @@ export default function Profile({ data }) {
   }
 
   return (
-    <div className='container profile'>
-      {data.map(profile => (
-        <div 
-          key={profile.id} 
-          className='card' 
-          onMouseEnter={() => setHover(true)} 
-          onMouseLeave={() => setHover(false)}
-        >
-          <img src={profile.avatar_url} alt="" />
-          <p><span className='label'>username:</span> {profile.login}</p>
-          {(
-          <>
-            <p><span className='label'>score:</span> {profile.score}</p>
-            <p><span className='label'>profile type:</span> {profile.type}</p>
-            <a className='link' href={profile.html_url}>View profile</a>
-          </>
-          )}
-          {totalPages !== page && <button className="btn-load-more" onClick={() => setPage(page + 1)}>{isPending ? 'loading...' : 'Load More'}</button>}
-        </div>
-      ))}
+    <div className='container'>
+      <div className="profile">
+        {data.map(profile => (
+          <div 
+            key={profile.id} 
+            className='card' 
+            onMouseEnter={() => setHover(true)} 
+            onMouseLeave={() => setHover(false)}
+          >
+            <img src={profile.avatar_url} alt="" />
+            <p><span className='label'>username:</span> {profile.login}</p>
+            {(
+            <>
+              <p><span className='label'>score:</span> {profile.score}</p>
+              <p><span className='label'>profile type:</span> {profile.type}</p>
+              <a className='link' href={profile.html_url}>View profile</a>
+            </>
+            )}
+          </div>
+        ))}
+      </div>
+      {totalPages !== page && <button className="btn-load-more" onClick={() => setPage(page + 1)}>{isPending ? 'loading...' : 'Load More'}</button>}
     </div>
+
   )
 }
