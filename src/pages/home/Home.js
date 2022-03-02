869498af2cd03f8dc47cdeedd2a63c1dd30a7258
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-03-02 09:58:38 +0100

    setPages

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,8 +1,8 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 // import { useFetch } from '../../hooks/useFetch'
 
 // components
-import Profile from '../../components/Profile'
+// import Profile from '../../components/Profile'
 
 // styles
 import './Home.css'
@@ -10,14 +10,14 @@ import './Home.css'
 
 export default function App() {
   // const { data, isPending, error } = useFetch('https://api.github.com/search/users?q=lagos&page=1')
-  const [pages, setPage] = useState(0)
+  const [page, setPage] = useState(0)
   const [totalPages, setTotalPages] = useState(0)
 
   const [users, setUsers] = useState([])
   const [isPending, setIsPending] = useState(false)
   const [error, setError] = useState(null)
 
-  const url = `https://api.github.com/search/users?q=lagos&page=1`
+  const url = `https://api.github.com/search/users?q=lagos&page=${page}`
 
   useEffect(() => {
     const getUser = async () => {
@@ -34,9 +34,11 @@ export default function App() {
         const data = await res.json()
         console.log(data)
 
-        setUsers(data)
         setError(false)
         setIsPending(false)
+        setUsers([...users, data.items])
+        setTotalPages(Math.round(data.total_count / 30))
+        console.log(totalPages, users)
       }
       catch(err) {
         setIsPending(false)
@@ -45,14 +47,14 @@ export default function App() {
       }
     }
     getUser()
-  }, [])
+  }, [url, page])
 
   
   return (
     <div className='container'>
       {error && <h2>{error}</h2>}
       {isPending && <h2>Loading...</h2>}
-      {/* {data && <Profile data={data} />} */}
+      {users && <Profile users={users} />}
     </div>
   );
 }
